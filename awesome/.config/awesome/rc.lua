76358c891faf156b3bd0d6ce5c40606f6b23cd7e
Kamiel de Visser <kamieldevisser@gmail.com>
2024-11-21 23:27:40 +0100

    change wezterm from flatpak

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -61,7 +61,7 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "flatpak run org.wezfurlong.wezterm"
+terminal = "wezterm"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
