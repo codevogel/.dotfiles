b6bdcabfc89dc561ac584b424d87c1d633b260e4
Kamiel de Visser <kamieldevisser@gmail.com>
2025-02-10 13:55:24 +0100

    add dep

--- install/curl/install-oh-my-posh.sh
+++ install/curl/install-oh-my-posh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-curl -s https://ohmyposh.dev/install.sh | sudo bash -s
+# Deps: zsh
+curl -s https://ohmyposh.dev/install.sh | sudo zsh -s
 mkdir -p ~/.poshthemes
 curl -s https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/patriksvensson.omp.json --output ~/.poshthemes/patriksvensson.omp.json
