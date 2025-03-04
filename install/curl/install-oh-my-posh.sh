cc1d0aca07709ed29382569a281b0de521b78b73
Kamiel de Visser <kamieldevisser@gmail.com>
2025-03-04 09:38:30 +0100

    update oh-my-posh install script

--- install/curl/install-oh-my-posh.sh
+++ install/curl/install-oh-my-posh.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Deps: zsh
-curl -s https://ohmyposh.dev/install.sh | sudo zsh -s
+curl -s https://ohmyposh.dev/install.sh | bash -s
 mkdir -p ~/.poshthemes
 curl -s https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/patriksvensson.omp.json --output ~/.poshthemes/patriksvensson.omp.json
