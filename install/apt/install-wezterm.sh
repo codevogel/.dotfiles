d838b3a57df3ad646098c9bc1dc1346f01158bfb
Kamiel de Visser <kamieldevisser@gmail.com>
2025-04-14 06:54:15 -0400

    add stow dep

--- install/apt/install-wezterm.sh
+++ install/apt/install-wezterm.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+# deps: stow
 # add apt repo
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 # install
 sudo apt install wezterm
 sudo apt update
+stow -d $HOME/.dotfiles/ -t $HOME -S wezterm
