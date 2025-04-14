64b6788a32a42e2949e27865a2950ee106122219
Kamiel de Visser <kamieldevisser@gmail.com>
2025-04-14 06:54:24 -0400

    add install-pls

---
+++ install/curl/install-pls.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# deps: stow, yq, fzf
+mkdir -p ~/work/pls
+curl -sS https://raw.githubusercontent.com/codevogel/pls/main/release/pls > ~/work/pls/pls && chmod +x ~/work/pls/pls
+stow -d $HOME/.dotfiles/ -t $HOME -S pls
