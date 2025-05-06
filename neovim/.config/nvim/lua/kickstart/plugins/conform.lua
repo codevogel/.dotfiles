4d368e9b2a481eb125464005bd14b4bac1ab0adc
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-06 14:34:08 +0200

    add prettier as svelte,javascript,typescript formatter

--- neovim/.config/nvim/lua/kickstart/plugins/conform.lua
+++ neovim/.config/nvim/lua/kickstart/plugins/conform.lua
@@ -34,7 +34,9 @@ return {
             --
             -- You can use a sub-list to tell conform to run *until* a formatter
             -- is found.
-            -- javascript = { { "prettierd", "prettier" } },
+            svelte = { 'prettier' },
+            javascript = { 'prettier' },
+            typescript = { 'prettier' },
          },
          formatters = {
             beautysh = {
