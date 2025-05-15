9ee4f9b6594f506748553e8993e0112b31f3948b
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-15 14:36:50 +0200

    tmux install

--- install/apt/install-tmux.sh
+++ install/apt/install-tmux.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# deps: stow, git
-sudo apt-get install tmux -y
+# deps: stow, git, libevent-dev, ncurses-dev, build-essential, bison, pkg-config
+
+sudo rm -rf /usr/local/bin/tmux
+wget https://github.com/tmux/tmux/releases/download/3.5a/tmux-3.5a.tar.gz
+tar -xzf tmux-3.5a.tar.gz
+cd tmux-3.5a
+./configure && make
+sudo make install
+# cleanup
+cd ..
+rm -rf tmux-3.5a
+rm tmux-3.5a.tar.gz
+
 stow -d $HOME/.dotfiles/ -t $HOME -S tmux
 
 # check if TPM is installed
