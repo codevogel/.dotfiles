e7e7c60e8cbdb7e17b1f97e9e04cef3e7ec11eb7
Kamiel de Visser <kamieldevisser@gmail.com>
2025-05-16 16:01:18 +0200

    change wezterm to wezterm nightly, and add its terminfo

--- install/apt/install-wezterm.sh
+++ install/apt/install-wezterm.sh
@@ -4,6 +4,12 @@
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 # install
-sudo apt install wezterm
+sudo apt install wezterm-nightly
 sudo apt update
 stow -d $HOME/.dotfiles/ -t $HOME -S wezterm
+
+# set terminfo so term=wezterm works
+tempfile=$(mktemp) \
+  && curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo \
+  && tic -x -o ~/.terminfo $tempfile \
+  && rm $tempfile
