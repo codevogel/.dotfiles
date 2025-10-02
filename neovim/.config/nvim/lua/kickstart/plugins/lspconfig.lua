508cc124a86e651f59ae05a0ca7f4ea55c1f8f1d
Kamiel de Visser <kamieldevisser@gmail.com>
2025-10-02 16:15:11 +0200

    upgrade deprecated lspconfig calls

--- neovim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ neovim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -145,7 +145,9 @@ return {
          --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
          local capabilities = vim.lsp.protocol.make_client_capabilities()
          capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-         require('lspconfig').gdscript.setup(capabilities)
+         vim.lsp.config('gdscript', {
+            capabilities = capabilities,
+         })
          -- Enable the following language servers
          --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
          --
@@ -217,11 +219,8 @@ return {
             handlers = {
                function(server_name)
                   local server = servers[server_name] or {}
-                  -- This handles overriding only values explicitly passed
-                  -- by the server configuration above. Useful when disabling
-                  -- certain features of an LSP (for example, turning off formatting for tsserver)
                   server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-                  require('lspconfig')[server_name].setup(server)
+                  vim.lsp.config(server_name, server)
                end,
             },
          }
