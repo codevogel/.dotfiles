50e8461cc62d6c30b28a38ee460e922278aee0e1
Kamiel de Visser <kamieldevisser@gmail.com>
2025-10-15 12:56:40 +0200

    remove nvim config

--- neovim/.config/nvim/lua/diagnostic.lua
+++
@@ -1,7 +0,0 @@
-vim.diagnostic.config {
-   -- Alternatively, customize specific options
-   virtual_lines = {
-      -- Only show virtual line diagnostics for the current cursor line
-      current_line = true,
-   },
-}
